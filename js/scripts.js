fe87d26eb02823f1bc3e98cb0cce66bb35b74dbe
FrankTimmons <franktimmonspdx@gmail.com>
2022-05-31 16:00:07 -0700

    Add monkeys in the shape of a smiley face, all clickable with jQuery
    Co-authored-by: Zachary Waggoner <github-zacwaggoner42@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 $(document).ready(function(){
+
   const sentenceInput = prompt("Enter a sentence: ")
   console.log(sentenceInput)
-  function constructSentence(letter1, letter2, sentence) {
-    return sentence + letter2 + letter1;
+
+  function constructSentence(str, sentence) {
+    return sentence + str;
   }
+
   function newSentence(sentence){
     firstLetter = sentence.slice(0,1).toUpperCase();
     lastLetter = sentence.slice(sentence.length - 1).toUpperCase();
-    return constructSentence(firstLetter, lastLetter, sentence);
+    return firstLetter + lastLetter;
   }
+
   function counting(sentence){
     const long = sentence.length/2;
     return sentence.slice(long, long + 1);
   } 
+
   function reverseString1(str) {
     var splitString = str.split("");
     var reverseArray = splitString.reverse();
     var joinArray = reverseArray.join("");
     return joinArray;
   }
+
   function popUp(){
-    const output = counting(sentenceInput) + newSentence(sentenceInput)
+    const output = counting(sentenceInput) + constructSentence(newSentence(sentenceInput), sentenceInput)
     newOutput = reverseString1(output);
   }
-  
+
   popUp()
 
   $("h1").click(function() {
@@ -33,4 +39,5 @@ $(document).ready(function(){
   $("img").click(function() {
     alert(newOutput);
   });
+  
 });
